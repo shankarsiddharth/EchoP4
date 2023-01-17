6f3e61beade305e722b456428d6c4723d68af3bb
Shankar Siddharth K A <7028991+shankarsiddharth@users.noreply.github.com>
2023-01-17 01:39:09 -0700

    Create Project Constants

---
+++ src/echo_p4_constants.py
@@ -0,0 +1,99 @@
+# Constants
+
+# ========================== START Application Constants ===================================
+# Application Folder Constants
+P4TOOLS_FOLDER_NAME = "P4Tools"
+BINARY_FOLDER_NAME = "bin"
+CONFIG_FOLDER_NAME = "config"
+CONFIG_DEFAULTS_FOLDER_NAME = "defaults"
+DATA_FOLDER_NAME = "data"
+DATA_DEFAULTS_FOLDER_NAME = "defaults"
+LOG_FOLDER_NAME = "logs"
+SOURCE_FOLDER_NAME = "src"
+TOOLS_FOLDER_NAME = "tools"
+TOOLS_DEFAULTS_FOLDER_NAME = "defaults"
+
+# Log File Constants
+LOG_FILE_NAME = "echo_p4.log"
+BACKUP_LOG_FILE_NAME = "echo_p4.backup.log"
+
+# DearPyGUI INI File Constants
+DEFAULT_DPG_INI_FILE_NAME = "dpg.default.ini"
+DPG_INI_FILE_NAME = "dpg.ini"
+
+# P4 INI File Constants
+DEFAULT_P4_INI_FILE_NAME = "p4.default.ini"
+P4_INI_FILE_NAME = "p4.ini"
+
+# Team Member JSON File Constants
+TEAM_MEMBERS_JSON_FILE_NAME = "team_members.json"
+GROUP_MEMBERS_JSON_FILE_NAME = "group_members.json"
+
+# P4 Custom Tools XML File Constants
+DEFAULT_P4_CUSTOM_TOOLS_XML_FILE_NAME = "echo_p4_tool.default.xml"
+P4_CUSTOM_TOOLS_XML_FILE_NAME = "echo_p4_tool.xml"
+
+# Echo P4 File Constants
+DEFAULT_ECHO_P4_INI_FILE_NAME = "echo.p4.default.ini"
+ECHO_P4_INI_FILE_NAME = "echo.p4.ini"
+
+DEFAULT_P4_IGNORE_FILE_NAME = ".p4ignore"
+
+# ========================== END Application Constants ===================================
+
+# ****************************************************************************************************************************************************************
+
+# ========================== START Config File Key Constants ===================================
+
+# Echo P4 Config Keys
+ECHO_P4_CONFIG_SECTION: str = "ECHO_P4_CONFIG"
+
+KEY_P4TOOLS_FOLDER_PATH: str = "P4TOOLS_FOLDER_PATH"
+KEY_BINARY_FOLDER_PATH: str = "BINARY_FOLDER_PATH"
+KEY_CONFIG_FOLDER_PATH: str = "CONFIG_FOLDER_PATH"
+KEY_CONFIG_DEFAULTS_FOLDER_PATH: str = "CONFIG_DEFAULTS_FOLDER_PATH"
+KEY_DATA_FOLDER_PATH: str = "DATA_FOLDER_PATH"
+KEY_DATA_DEFAULTS_FOLDER_PATH: str = "DATA_DEFAULTS_FOLDER_PATH"
+KEY_LOG_FOLDER_PATH: str = "LOG_FOLDER_PATH"
+KEY_SOURCE_FOLDER_PATH: str = "SOURCE_FOLDER_PATH"
+KEY_TOOLS_FOLDER_PATH: str = "TOOLS_FOLDER_PATH"
+KEY_TOOLS_DEFAULTS_FOLDER_PATH: str = "TOOLS_DEFAULTS_FOLDER_PATH"
+
+KEY_LOG_FILE_PATH: str = "LOG_FILE_PATH"
+KEY_BACKUP_LOG_FILE_PATH: str = "BACKUP_LOG_FILE_PATH"
+
+KEY_DEFAULT_DPG_INI_FILE_PATH: str = "DEFAULT_DPG_INI_FILE_PATH"
+KEY_DPG_INI_FILE_PATH: str = "DPG_INI_FILE_PATH"
+
+KEY_DEFAULT_P4_INI_FILE_PATH: str = "DEFAULT_P4_INI_FILE_PATH"
+KEY_P4_INI_FILE_PATH: str = "P4_INI_FILE_PATH"
+
+KEY_TEAM_MEMBERS_JSON_FILE_PATH: str = "TEAM_MEMBERS_JSON_FILE_PATH"
+
+KEY_DEFAULT_P4_CUSTOM_TOOLS_XML_FILE_PATH: str = "DEFAULT_P4_CUSTOM_TOOLS_XML_FILE_PATH"
+KEY_P4_CUSTOM_TOOLS_XML_FILE_PATH: str = "P4_CUSTOM_TOOLS_XML_FILE_PATH"
+
+# P4 Config Keys
+P4_CONFIG_SECTION: str = "P4_CONFIG"
+KEY_P4PORT: str = "p4port"
+KEY_P4USER: str = "p4user"
+KEY_P4CLIENT: str = "p4client"
+KEY_P4IGNORE: str = "p4ignore"
+KEY_P4PASSWD: str = "p4passwd"
+
+# ========================== END Config File Key Constants ===================================
+
+# ****************************************************************************************************************************************************************
+
+# ========================== START DearPyGUI's Viewport Constants ===================================
+
+ECHO_P4_TOOL_WINDOW_TITLE: str = "Echo P4 Tool"
+ECHO_P4_COMMAND_WINDOW: str = "Echo P4 Commands"
+FILE_MENU: str = "File"
+SAVE_CURRENT_LAYOUT_TO_DPG_INI: str = "Save Current Layout"
+RESET_TO_DEFAULT_LAYOUT: str = "Reset to Default Layout"
+
+# ========================== END DearPyGUI's Viewport Constants ===================================
+
+
+# ****************************************************************************************************************************************************************
